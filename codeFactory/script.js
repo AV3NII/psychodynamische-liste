539cf31d78a404d14f99131d9e23023687c5fe79
shawn-ii <106849147+shawn-ii@users.noreply.github.com>
2022-06-04 02:33:48 +0200

    init

---
+++ codeFactory/script.js
@@ -0,0 +1,26 @@
+
+window.onload=function(){
+const dropdown = document.querySelector('.dropdown');
+const dropNav = document.getElementById('dropNav');
+
+
+dropdown.addEventListener('click', function (){
+    this.classList.toggle('isActive');
+    dropNav.classList.toggle('isActive');
+});
+}
+
+
+function colorInvert(){
+
+    var el = document.documentElement.classList;
+    el.toggle("dark")
+
+}
+
+
+
+
+
+
+
