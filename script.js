06eda4660ab859917ce09fe5cc58a09d23666044
shawn-ii <106849147+shawn-ii@users.noreply.github.com>
2022-06-05 00:51:06 +0200

    image-color grading

--- script.js
+++ script.js
@@ -4,15 +4,18 @@ const dropdown = document.querySelector('.dropdown');
 const dropNav = document.getElementById('dropNav');
 
 
+
 dropdown.addEventListener('click', function (){
     this.classList.toggle('isActive');
     dropNav.classList.toggle('isActive');
 });
+
+
 }
 
 
 function colorInvert(){
-
+    
     var el = document.documentElement.classList;
     el.toggle("dark")
 
